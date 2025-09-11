52b377d6c21587adfc4dafe040199c88e7da9807
Mit-007 <157833261+Mit-007@users.noreply.github.com>
2025-09-11 20:01:43 +0530

    Create A. Collatz Conjecture.cpp

---
+++ Codeforces - Contest/Codeforces Round 1047 (Div. 3)/A. Collatz Conjecture.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+
+#define NL endl
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vii vector<pair<int,int>>
+#define pii pair<int,int>
+
+
+void m_j_007(){
+	int k,x;
+	cin>>k>>x;
+	while(k--){
+		x*=2;
+	}
+	cout<<x<<endl;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		m_j_007();
+	}
+	return 0;
+}
