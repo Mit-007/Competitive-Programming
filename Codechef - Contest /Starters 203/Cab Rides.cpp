9d4c855d400b8e115dff53d742dfc98478008bbc
Mit-007 <157833261+Mit-007@users.noreply.github.com>
2025-09-11 19:37:02 +0530

    Create Cab Rides.cpp

---
+++ Codechef - Contest /Starters 203/Cab Rides.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// m_j_007
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    if(n==1) cout<<200<<endl;
+    else cout<<100*n<<endl;
+  }
+}
