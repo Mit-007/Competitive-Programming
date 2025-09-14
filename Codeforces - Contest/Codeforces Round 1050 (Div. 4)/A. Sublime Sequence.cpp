27cc89bddc61cb4ebac1d6136133910062ec94c2
Mit-007 <157833261+Mit-007@users.noreply.github.com>
2025-09-14 08:20:47 +0530

    Create A. Sublime Sequence.cpp

---
+++ Codeforces - Contest/Codeforces Round 1050 (Div. 4)/A. Sublime Sequence.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <climits> 
+using namespace std;
+
+#define NL endl
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vvstr vector<vector<string>>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vpii vector<pair<int,int>>
+#define pii pair<int,int>
+#define si set<int>
+#define sll set<ll>
+const int mod=1e9+7;
+
+// solution 
+
+void m_j_007(){
+	int x,n;
+	cin>>x>>n;
+	if(n%2==0) cout<<0<<endl;
+	else cout<<x<<endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		m_j_007();
+	}
+
+	return 0;
+}
